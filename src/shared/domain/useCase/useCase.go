621d97b0f47189b9b99f0a0bb54e324679964cb8
Alejandro Garcia Serna <alejandrogarcia@seven4n.com>
2021-03-16 11:54:17 -0500

    Add comments to event scheduler struct

--- src/shared/domain/useCase/useCase.go
+++ src/shared/domain/useCase/useCase.go
@@ -2,18 +2,23 @@ package usecase
 
 import "github.com/alejogs4/hn-website/src/shared/domain/domainevent"
 
+// UseCase interface
 type UseCase interface {
 	RegisterEventHandler(eventName string, handler domainevent.DomainEventHandler)
 }
 
+// EventScheduler struct define methods to register domain event and its handlers, those which will be executed when a
+// meaningful domain even occurs
 type EventScheduler struct {
 	handlers map[string][]domainevent.DomainEventHandler
 }
 
+// NewEventScheduler .
 func NewEventScheduler() EventScheduler {
 	return EventScheduler{handlers: make(map[string][]domainevent.DomainEventHandler)}
 }
 
+// Handlers returns non exported handlers field
 func (uc *EventScheduler) Handlers() map[string][]domainevent.DomainEventHandler {
 	return uc.handlers
 }
